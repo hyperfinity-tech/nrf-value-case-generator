1692a030df611a4b0b89072882b9f471a39b75f9
tom-hyper <106238503+tom-hyper@users.noreply.github.com>
2025-12-11 12:49:48 +0000

    make section wider

--- components/abm-page-client.tsx
+++ components/abm-page-client.tsx
@@ -68,7 +68,7 @@ export function AbmPageClient({
             </p>
           </div>
 
-          <div className="max-w-4xl mx-auto">
+          <div className="max-w-6xl mx-auto">
             <ABMPackGenerator
               brandingOverride={{
                 toolName: branding.toolName,
